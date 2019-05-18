4533e5335fbbc276ade4367391477c281398eca2
Memhir-YasuE <habesha787@gmail.com>
2019-05-18 12:50:39 -0400

    comment changes

--- MultiVariateLinearReg.py
+++ MultiVariateLinearReg.py
@@ -6,31 +6,30 @@ class MultiVarLinearReg:
         self.target = np.array(target)
 
     def normal_eq(self):
-        X = (self.features).T # Applied transposition for the sake of following along with the normal equation
+        # Applied transposition for the sake of following along with the normal equation when applying matrix operations
+        X = (self.features).T
         y = self.target
 
         # left half of normal equation
         left = np.linalg.inv(np.dot(X.T,X))
         #right half of normal equation
         right = np.dot(X.T, y)
-        # returns thetha
+        # returns a vector of theta in the form of a list
         return np.dot(left,right)
 
-    def cost_function(self):
+    def predict(self):
         X = (self.features)
         actual = self.target
         theta_vector = self.normal_eq()
-        hypothesis = np.dot(theta_vector,X)
-        loss = hypothesis - actual
-        return hypothesis, loss
+        prediction = np.dot(theta_vector,X)
+        # loss = prediction - actual
+        return prediction
 
-# house size, num of bed rooms
 
-theta = np.array([1,2])
-X = np.array([1,2000])
 
-                          # Features: House size, num of bed room
+
+
+                          # Features: [House size], [# of bed room]   # target: sale price (1000x)
 model = MultiVarLinearReg(features=[[2104,1496,1534,852],[5,3,3,2]], target = [460,232,315,178])
 model.normal_eq()
-
-model.cost_function()
+print(model.predict())
